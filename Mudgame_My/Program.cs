f438c62763ecb1f0f18bc1cb5f4cc6256a1e1c2b
mayongpv <82883541+mayongpv@users.noreply.github.com>
2021-05-11 20:37:22 +0900

    초기 커밋.

---
+++ Mudgame_My/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mudgame_My
+{
+    class Program
+    {
+        static Random random = new Random();
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
